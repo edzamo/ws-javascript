0e369d4c7ebc326bfb3161a81d534361072a8038
Edwin Zamora <ezamora@Elvins-MacBook-Pro.local>
2025-03-20 19:26:47 -0500

    initial concept

---
+++ course-openwebinar/3.-dynamicTyping.js
@@ -0,0 +1,24 @@
+//Dynamic Typing
+let isActive="1";
+console.log(typeof isActive);
+
+isActive = true;
+
+console.log(typeof isActive);
+
+
+isActive=1;
+
+console.log(typeof isActive);
+
+//Casting
+console.log('-----Casting----')
+
+let otherIsActive = "1";
+console.log(typeof isActive);
+
+isActive = Boolean(isActive);
+
+console.log(typeof isActive)
+
+//
